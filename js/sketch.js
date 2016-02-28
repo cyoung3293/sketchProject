22e701a5d58e484adeca3c7ad062d5cbdf6c9c72
Christian A. Young <c.young3293@gmail.com>
2016-02-28 11:59:39 -0500

    finished portion 1

--- js/sketch.js
+++ js/sketch.js
@@ -1,19 +1,50 @@
 $(document).ready(function() {
-gridWidth = 2;
-gridHeight = 1;
+
+gridWidth = prompt("Please enter grid width.", "16");
+gridHeight = prompt("Please enter grid height.", "16");
+
 countX = 1;
 countY = 1;
-originalWidth = 996;
-originalHeight = 996;
-newWidth = (996 - (2+2*gridWidth))/(gridWidth);
-newHeight = (996 - (2+2*gridHeight))/(gridHeight);
+
+newWidth = (500 - (2+2*gridWidth))/(gridWidth);
+newHeight = (500 - (2+2*gridHeight))/(gridHeight);
+
+initializer = [];
+while (countY <= (gridHeight-1)) {
+	if (countY === 1) {
+	initializer.push("<div class='clear'></div><div class='initializer grid top'></div><div class='clear'></div>")
+	countY += 1;
+     }
+     else {
+     	initializer.push("<div class='initializer grid'></div><div class='clear'></div>")
+	countY += 1;
+     }
+}
+$.each(initializer, function(i, val) {
+	$("div.wrapper").append(val)
+})
+
+row = [];
+while (countX <= (gridWidth-1)) {
+	row.push("<div class='grid'></div>");
+	countX += 1;
+}
 
 
 
 
-$("div.wrapper").append("<div class='grid'></div>");
+$.each(row, function(i, val) {
+	$("div.initializer").after(val);
+});
 
 $(".grid").height(newHeight);
 $(".grid").width(newWidth);
+
+
+$(".grid").mouseenter(function() {
+	$(this).css("background-color","red");
+});
+
+
 
 });
